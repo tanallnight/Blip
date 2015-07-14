05c65190f16c0d72d98e04f4bd7525e0aba6f184
Tanmay Parikh <tanpar1995@gmail.com>
2015-07-14 12:59:56 -0400

    User has to click button before download begins

--- app/src/main/java/com/tanmay/blip/activities/DownloadActivity.java
+++ app/src/main/java/com/tanmay/blip/activities/DownloadActivity.java
@@ -8,19 +8,26 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.tanmay.blip.R;
 import com.tanmay.blip.networking.XKCDDownloader;
 
-public class DownloadActivity extends AppCompatActivity {
+public class DownloadActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private ProgressBar progressBar;
+    private TextView progress, title, pageTitle, caption;
     public BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             switch (intent.getAction()) {
                 case XKCDDownloader.DOWNLOAD_PROGRESS:
+                    if (progressBar.isIndeterminate()) {
+                        progressBar.setIndeterminate(false);
+                    }
                     progressBar.setProgress((int) intent.getExtras().getDouble(XKCDDownloader.PROGRESS));
                     progress.setText(String.valueOf(intent.getExtras().getDouble(XKCDDownloader.PROGRESS)) + "%");
                     title.setText(intent.getExtras().getString(XKCDDownloader.TITLE));
@@ -48,9 +55,8 @@ public class DownloadActivity extends AppCompatActivity {
             }
         }
     };
-
-    private ProgressBar progressBar;
-    private TextView progress, title;
+    private Button button;
+    private View downloadUI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,10 +64,21 @@ public class DownloadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_download);
         progress = (TextView) findViewById(R.id.progress);
         title = (TextView) findViewById(R.id.titles);
+        pageTitle = (TextView) findViewById(R.id.title);
+        caption = (TextView) findViewById(R.id.caption);
+        button = (Button) findViewById(R.id.start_download);
+        downloadUI = findViewById(R.id.download_ui);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        progressBar.setIndeterminate(false);
 
+        downloadUI.setVisibility(View.GONE);
+        pageTitle.setText(getResources().getString(R.string.action_download_all_comics_explain));
+        caption.setText(getResources().getString(R.string.action_download_all_comics_explain_caption));
+        button.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(XKCDDownloader.DOWNLOAD_PROGRESS);
         intentFilter.addAction(XKCDDownloader.DOWNLOAD_SUCCESS);
@@ -71,5 +88,11 @@ public class DownloadActivity extends AppCompatActivity {
         Intent intent = new Intent(this, XKCDDownloader.class);
         intent.setAction(XKCDDownloader.DOWNLOAD_ALL);
         startService(intent);
+
+        pageTitle.setText(getResources().getString(R.string.action_download_all_comics));
+        caption.setText(getResources().getString(R.string.action_download_all_comics_caption));
+        button.setVisibility(View.GONE);
+        downloadUI.setVisibility(View.VISIBLE);
+        progressBar.setIndeterminate(true);
     }
 }
